22ed7ebca30350a8440cd39ff704d4cef7730b66
Praveena25 <66820977+Praveena25@users.noreply.github.com>
2020-06-24 14:00:51 +0530

    Add files via upload

---
+++ inheritance/Animal.java
@@ -0,0 +1,11 @@
+
+public class Animal {
+public void eat() {
+	System.out.println("eatingg");
+
+	}
+public void sleep() {
+	System.out.println("sleepingg");
+}
+
+}
---
+++ inheritance/Main.java
@@ -0,0 +1,16 @@
+
+public class main {
+
+	public static void main(String[] args) {
+      Bird b=new Bird();
+      System.out.println("birds will be.............");
+      b.eat();
+      b.fly();
+      b.sleep();
+      Animal a=new Animal();
+      System.out.println("Animals will be.............");
+      a.eat();
+      a.sleep();
+	}
+
+}
